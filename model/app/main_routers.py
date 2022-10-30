b2e523236fd0d903c4affe8f7047811abff74738
truonghm <truong173@outlook.com>
2022-10-31 00:51:28 +0700

    update test model module import

--- model/app/main_routers.py
+++ model/app/main_routers.py
@@ -1,4 +1,4 @@
-from app.modules.wine.routers import router as wine_router
+from app.modules.test_model.routers import router as wine_router
 from app.modules.healthcheck.routers import router as hc_router
 
 def add_all_routers(app):
