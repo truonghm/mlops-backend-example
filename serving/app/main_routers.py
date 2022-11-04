1f000f12057e629fc8dadeb63848a7a103e5aeba
truonghm <truong173@outlook.com>
2022-11-05 03:11:42 +0700

    modify instrumentator and add error handler

--- serving/app/main_routers.py
+++ serving/app/main_routers.py
@@ -1,9 +1,7 @@
-from app.modules.test_model.routers import router as wine_router
 from app.modules.healthcheck.routers import router as hc_router
 from app.modules.quantity.routers import router as quantity_router
 
 def add_all_routers(app):
 
-    app.include_router(wine_router, prefix="/wine", tags=["wine"])
     app.include_router(hc_router, prefix="/healthcheck", tags=["test"])
     app.include_router(quantity_router, prefix="/quantity", tags=["quantity"])
--- serving/main.py
+++ serving/main.py
@@ -1,23 +1,29 @@
-import os
-from pathlib import Path
+# import os
+# from pathlib import Path
 
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
-import uvicorn, time
-import aioredis
+import uvicorn
+# import time
+# import aioredis
 from uvicorn.config import LOGGING_CONFIG
 
 from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from app.main_routers import add_all_routers
 from app.base.config import settings
-# from app.base.monitoring import instrumentator
-from starlette_prometheus import metrics, PrometheusMiddleware
+from app.base.errors import CustomException
+from prometheus_fastapi_instrumentator import Instrumentator
 
-from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend
 
-# from app.base.databases import SessionLocal, engine
+def init_listeners(app: FastAPI) -> None:
+    @app.exception_handler(CustomException)
+    async def custom_exception_handler(request: Request, exc: CustomException):
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"error_code": exc.code, "message": exc.msg}
+        )
 
 app = FastAPI(
     title="Pod Foods ML APIs",
@@ -26,43 +32,14 @@ app = FastAPI(
     docs_url="/documentation",
     redoc_url="/redoc",
 )
-
+init_listeners(app)
 add_all_routers(app)
-app.add_middleware(PrometheusMiddleware)
-app.add_route("/metrics", metrics)
-
-@app.middleware("http")
-async def add_process_time_header(request: Request, call_next):
-    start_time = time.time()
-    response = await call_next(request)
-    process_time = time.time() - start_time
-    response.headers["X-Process-Time"] = str(process_time)
-    return response
 
 @app.on_event("startup")
-async def startup():
-    redis =  aioredis.from_url(settings.CELERY_BROKER_URL, encoding="utf8", decode_responses=True)
-    FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
-
-
-
-# from multiprocessing import Process
-# import subprocess
-
-# def run_celery():
-#     # subprocess.run('celery -A app.worker.celery_app worker -l INFO -c {}'.format(settings.NUMBER_OF_WORKER), shell=True)
-#     # subprocess.run('celery -A app.worker.celery_app worker -B -l INFO -c {} --without-gossip --without-mingle --without-heartbeat -Ofair'.format(settings.NUMBER_OF_WORKER), shell=True)
-
-#     # run this on windows
-#     subprocess.run('redis-server', shell=True) 
-#     subprocess.run('celery -A app.worker.celery_app worker -E -l INFO -c {} --without-gossip --without-mingle --without-heartbeat -Ofair --pool=gevent'.format(settings.NUMBER_OF_WORKER), shell=True)
-#     # celery -A app.worker.celery_app worker -B -l info 
-
+async def startup_event():
+    Instrumentator().instrument(app=app).expose(app, include_in_schema=False, should_gzip=True)
 
 if __name__ == "__main__":
-    # subprocess.run('set PATH=%PATH%;C:\Users\hai.dao_onemount\instantclient_21_3', shell=True)
-    # celery_process = Process(target=run_celery, args=())
-    # celery_process.start()
     LOGGING_CONFIG["formatters"]["access"]["fmt"] = '%(asctime)s %(levelprefix)s %(client_addr)s - "%(request_line)s" %(status_code)s'
     LOGGING_CONFIG["formatters"]["access"]["datefmt"] = '%Y-%m-%d %H:%M:%S'
 
